f5c74ac1c18ac36305d068507f87618106772169
pavlosymonov <simonov.pasha9822@gmail.com>
2021-08-11 09:08:20 +0300

    Simple TCP client server

---
+++ simpleTCP/tcp-client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+	 	reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(conn, text + "\n")
+
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print("Message from server: "+message)
+	}
+}
